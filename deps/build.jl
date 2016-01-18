ace71caf332b27a65df7ef1fef7b0a654b8dc202
Jonathan Goldfarb <jgoldfar@gmail.com>
2016-01-18 13:17:54 -0500

    Build using Homebrew rather than Conda on OSX

--- deps/build.jl
+++ deps/build.jl
@@ -3,8 +3,15 @@ using BinDeps
 @BinDeps.setup
 libnetcdf = library_dependency("libnetcdf", aliases = ["libnetcdf4","libnetcdf-7,netcdf"])
 
-using Conda
-provides(Conda.Manager, "libnetcdf", libnetcdf)
+@windows_only begin
+  using Conda
+  provides(Conda.Manager, "libnetcdf", libnetcdf)
+end
+
+@osx_only begin
+  using Homebrew
+  provides(Homebrew.HB, "netcdf", libnetcdf, os = :Darwin)
+end
 
 provides(AptGet, Dict("libnetcdf-dev"=>libnetcdf), os = :Linux)
 provides(Yum, Dict("netcdf-devel"=>libnetcdf), os = :Linux)
